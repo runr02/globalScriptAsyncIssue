dad719b1d74d8507c098118115bb0f8926bec222
Medina <moises.d.medina@metlife.com>
2023-10-03 14:07:39 -0400

    global script async issue

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -6,7 +6,7 @@ import { format } from '../../utils/utils';
   styleUrl: 'my-component.css',
   shadow: true,
 })
-export class MyComponent {
+export class MyComponent  {
   /**
    * The first name
    */
@@ -26,7 +26,16 @@ export class MyComponent {
     return format(this.first, this.middle, this.last);
   }
 
+  async componentWillRender(){
+    let cnt = 0; 
+    while(cnt != 100){
+      console.log("Component will render!"); 
+      cnt++; 
+    }
+  }
+
   render() {
+    console.log("Component rendered!");
     return <div>Hello, World! I'm {this.getText()}</div>;
   }
 }
--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'stencil-starter-project-name',
+  globalScript: 'src/global/init.ts',
   outputTargets: [
     {
       type: 'dist',
